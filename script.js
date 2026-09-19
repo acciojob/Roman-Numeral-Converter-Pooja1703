63428b47d27803d034d91bd1963939c069274d4f
Pooja1703 <129085948+Pooja1703@users.noreply.github.com>
2026-09-19 14:02:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 function convertToRoman(num) {
-  const obj = {
-    0:['M',1000], 
-    1:['D', 500], 
-    2:['C', 100], 
-    3:['L', 50], 
-    4:['X', 10], 
-    5:['V', 5], 
-    6:['I', 1]
-  };
+  const obj = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
+  ];
 
   let ans = "";
 
-  for(let i = 0; i < 7; i++){
+  for(let i=0; i<obj.length; i++){
     while(num >= obj[i][1]){
       ans += obj[i][0];
       num -= obj[i][1];
@@ -20,6 +26,3 @@ function convertToRoman(num) {
 
   return ans;
 }
-
-
-module.exports = convertToRoman;
